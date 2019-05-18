7ed71db45c432070dce06d4822868ab35c7040e4
Rizky Adipratama <adipratama.rizky@gmail.com>
2019-05-18 15:46:45 +0700

    Tampilan UI

---
+++ NanoChallenge1Project/PlayAreaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlayAreaViewController.swift
+//  NanoChallenge1Project
+//
+//  Created by Rizky Adipratama Ruddyar on 18/05/19.
+//  Copyright © 2019 Rizky Adipratama Ruddyar. All rights reserved.
+//
+
+import UIKit
+
+class PlayAreaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
